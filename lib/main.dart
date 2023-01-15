809baecdde9459f8acf3da35047fdc0e7a3fd562
esraaabdrabo <esraaabdrabo23@gmail.com>
2023-01-15 22:25:08 +0200

    end

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,6 @@ class PlayController extends StatefulWidget {
 class _PlayControllerState extends State<PlayController> {
   @override
   Widget build(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width;
     var screenHeight = MediaQuery.of(context).size.height;
 
     return ChangeNotifierProvider(
@@ -42,100 +41,49 @@ class _PlayControllerState extends State<PlayController> {
       builder: (context, child) {
         PlayControllerVM provider = Provider.of(context);
         return Scaffold(
-            backgroundColor: Colors.black,
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                //show the moveBTN image or empty space
-                provider.result != ""
-                    ? Image.asset(
-                        provider.result,
-                        height: screenHeight * .15,
-                      )
-                    : SizedBox(
-                        height: screenHeight * .15,
-                      ),
-                CircleAvatar(
-                  radius: screenWidth * .15,
-                  backgroundColor: Colors.transparent,
-                  child: Container(
-                    padding: EdgeInsets.all(screenHeight * .01),
-                    height: screenHeight * .4,
-                    width: screenHeight * .4,
-                    decoration: BoxDecoration(
-                        gradient: RadialGradient(
-                            colors: [greyColor, Colors.white],
-                            radius: 7,
-                            center: Alignment.bottomCenter),
-                        boxShadow: const [
-                          BoxShadow(
-                              offset: Offset(-3, -3),
-                              color: Color.fromARGB(255, 93, 93, 93),
-                              blurRadius: 3),
-                          BoxShadow(
-                              offset: Offset(3, 3),
-                              color: Color.fromARGB(255, 93, 93, 93),
-                              blurRadius: 3)
-                        ],
-                        shape: BoxShape.circle),
-                    child: Stack(
-                      alignment: AlignmentDirectional.center,
+          backgroundColor: Colors.black,
+          body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              return constraints.maxWidth > 700
+                  ?
+                  //lab
+                  Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        provider.showMoves
-                            ? Stack(
-                                children: [
-                                  //triangle
-                                  MoveBTN(
-                                    provider: provider,
-                                    moveBTNIndex: 1,
-                                    imgPath: "lib/images/triangle.png",
-                                    align: Alignment.topCenter,
-                                  ),
-                                  //x
-                                  MoveBTN(
-                                    provider: provider,
-                                    moveBTNIndex: 3,
-                                    imgPath: "lib/images/x.png",
-                                    align: Alignment.bottomCenter,
-                                  ),
-                                  //circle
-                                  MoveBTN(
-                                    provider: provider,
-                                    moveBTNIndex: 0,
-                                    imgPath: "lib/images/circle.png",
-                                    align: Alignment.centerLeft,
-                                  ),
-                                  //square
-                                  MoveBTN(
-                                    provider: provider,
-                                    moveBTNIndex: 2,
-                                    imgPath: "lib/images/square.png",
-                                    align: Alignment.centerRight,
-                                  ),
-                                ],
+                        //show the moveBTN image or empty space
+                        provider.result != ""
+                            ? Image.asset(
+                                provider.result,
+                                width: screenHeight * .5,
                               )
-                            : const SizedBox(),
-                        Draggable(
-                            onDragStarted: () {
-                              provider.changeShowMoves(true);
-                            },
-                            onDraggableCanceled: (velocity, offset) {
-                              provider.changeShowMoves(false);
-                            },
-                            data: "",
-                            rootOverlay: true,
-                            //ال ويدجت الاساسية هتتبدل بدة لما يتسحب
-                            childWhenDragging: const SizedBox(),
-                            feedback: const ControllerBTN(),
-                            child: const ControllerBTN()),
+                            : SizedBox(
+                                width: screenHeight * .5,
+                              ),
+                        Controller(provider),
                       ],
-                    ),
-                  ),
-                ) //x
-                ,
-              ],
-            ));
+                    )
+                  : Column(
+                      // crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        //show the moveBTN image or empty space
+                        provider.result != ""
+                            ? Image.asset(
+                                provider.result,
+                                height: screenHeight * .15,
+                              )
+                            : SizedBox(
+                                height: screenHeight * .15,
+                              ),
+                        SizedBox(
+                            height: screenHeight * .7,
+                            child: Controller(provider)),
+                      ],
+                    );
+            },
+          ),
+        );
       },
     );
   }
@@ -156,9 +104,6 @@ class MoveBTN extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width;
-    var screenHeight = MediaQuery.of(context).size.height;
-
     return Align(
       alignment: align,
       child: DragTarget(
@@ -171,7 +116,7 @@ class MoveBTN extends StatelessWidget {
 
         builder: (context, acceptedData, rejectedData) {
           return Container(
-            padding: EdgeInsets.all(screenHeight * .015),
+            padding: const EdgeInsets.all(15),
             decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
               const BoxShadow(
                   offset: Offset(3, 3), color: Colors.black, blurRadius: 5),
@@ -179,13 +124,101 @@ class MoveBTN extends StatelessWidget {
                   offset: Offset(-3, -3), color: Colors.black, blurRadius: 5),
               BoxShadow(offset: const Offset(1, 1), color: greyColor),
             ]),
-            height: screenHeight * .1,
-            width: screenWidth * .05,
+            height: 80,
+            width: 80,
             child: Image.asset(imgPath),
           );
         },
       ),
     );
+  }
+}
+
+class Controller extends StatelessWidget {
+  PlayControllerVM provider;
+  Controller(this.provider, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    return CircleAvatar(
+      radius: 300, //screenWidth * .15,
+      backgroundColor: Colors.transparent,
+      child: Container(
+        padding: EdgeInsets.all(screenHeight * .01),
+        height: 250, // screenHeight * .4,
+        width: 250, //screenHeight * .4,
+        decoration: BoxDecoration(
+            gradient: RadialGradient(
+                colors: [greyColor, Colors.white],
+                radius: 7,
+                center: Alignment.bottomCenter),
+            boxShadow: const [
+              BoxShadow(
+                  offset: Offset(-3, -3),
+                  color: Color.fromARGB(255, 93, 93, 93),
+                  blurRadius: 3),
+              BoxShadow(
+                  offset: Offset(3, 3),
+                  color: Color.fromARGB(255, 93, 93, 93),
+                  blurRadius: 3)
+            ],
+            shape: BoxShape.circle),
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            provider.showMoves
+                ? Stack(
+                    children: [
+                      //triangle
+                      MoveBTN(
+                        provider: provider,
+                        moveBTNIndex: 1,
+                        imgPath: "lib/images/triangle.png",
+                        align: Alignment.topCenter,
+                      ),
+                      //x
+                      MoveBTN(
+                        provider: provider,
+                        moveBTNIndex: 3,
+                        imgPath: "lib/images/x.png",
+                        align: Alignment.bottomCenter,
+                      ),
+                      //circle
+                      MoveBTN(
+                        provider: provider,
+                        moveBTNIndex: 0,
+                        imgPath: "lib/images/circle.png",
+                        align: Alignment.centerLeft,
+                      ),
+                      //square
+                      MoveBTN(
+                        provider: provider,
+                        moveBTNIndex: 2,
+                        imgPath: "lib/images/square.png",
+                        align: Alignment.centerRight,
+                      ),
+                    ],
+                  )
+                : const SizedBox(),
+            Draggable(
+                onDragStarted: () {
+                  provider.changeShowMoves(true);
+                },
+                onDraggableCanceled: (velocity, offset) {
+                  provider.changeShowMoves(false);
+                },
+                data: "",
+                rootOverlay: true,
+                //ال ويدجت الاساسية هتتبدل بدة لما يتسحب
+                childWhenDragging: const SizedBox(),
+                feedback: const ControllerBTN(),
+                child: const ControllerBTN()),
+          ],
+        ),
+      ),
+    ) //x
+        ;
   }
 }
 
@@ -200,8 +233,9 @@ class ControllerBTN extends StatelessWidget {
     return
         //shadow effect
         Container(
-      height: screenHeight * .09,
-      width: screenWidth * .035,
+      height: 50, // screenHeight * .09,
+      width: 50 //screenWidth * .035,
+      ,
       decoration: BoxDecoration(boxShadow: [
         const BoxShadow(
             offset: Offset(3, 3), color: Colors.black, blurRadius: 5),
@@ -211,8 +245,8 @@ class ControllerBTN extends StatelessWidget {
       ], shape: BoxShape.circle, color: const Color.fromARGB(255, 82, 82, 82)),
       //gradient effect
       child: Container(
-        height: screenHeight * .05,
-        width: screenWidth * .02,
+        height: 35, // screenHeight * .05,
+        width: 35, //screenWidth * .02,
         decoration: BoxDecoration(
             shape: BoxShape.circle,
             gradient: RadialGradient(
